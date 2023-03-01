dd3955e3c73c3de8a43b78457b97eaba127e3f31
rehanarroihan <third2014project@gmail.com>
2023-03-01 15:15:38 +0700

    creating auth login

---
+++ lib/screens/auth/login_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:hrtodo/screens/hr/task_list.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfffafafa),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w500
+                ),
+              ),
+              const SizedBox(height: 24),
+
+              const TextField(
+                decoration: InputDecoration(
+                  hintText: "Username",
+                  labelText: "Username",
+                  prefix: Padding(
+                    padding: EdgeInsets.only(right: 5),
+                    child: Icon(
+                      Icons.person,
+                      size: 16,
+                    ),
+                  ),
+                  prefixIconConstraints: BoxConstraints(minHeight: 100),
+                )
+              ),
+
+              const SizedBox(height: 16),
+
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: "Password",
+                )
+              ),
+
+              const SizedBox(height: 12),
+
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) => TaskList()
+                    ));
+                  },
+                  child: Text('Logins')
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
